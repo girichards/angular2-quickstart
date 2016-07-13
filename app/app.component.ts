fc2dd619084f9355fd8dc0840e8a8b4ae2da805c
Gary Richards <gary.i.richards@gmail.com>
2016-07-13 15:55:14 +0100

    ngModel mods

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template:`
+  <h1>{{title}}</h1>
+  <div><label>id: </label>{{hero.id}}</div>
+  <h2>{{hero.name}} details!</h2>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
