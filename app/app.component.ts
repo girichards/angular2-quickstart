f446509813416ca1d4fb6c08aa89da4fcb7ea347
Gary Richards <gary.richards@activestandards.com>
2016-07-14 11:04:26 +0100

    4. Multiple components...

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 @Component({
   selector: 'my-app',
+  directives: [HeroDetailComponent],
   template:`
   <h1>{{title}}</h1>
-
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
-
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
   .selected {
@@ -77,10 +71,7 @@ export class AppComponent {
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
-export class Hero {
-  id: number;
-  name: string;
-}
+
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
